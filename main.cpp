74a18c61d908b777e84dc50e69c1b1e3f607059f
ArnauUstrell <61042923+ArnauUstrell@users.noreply.github.com>
2020-02-21 09:58:41 +0100

    Primers pasos HelloSDL
    Primers passos del HelloSDL

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include "SDL/include/SDL.h"
+#pragma comment(lib,"SDL/libx86/SDL2.lib")
+#pragma comment(lib,"SDL/libx86/SDL2main.lib")
+#include <iostream>
+
+int main(int argc, char** argv) {
+	
+	//STARTING SDL
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	//OPENING A WINDOW
+	SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	if (win == nullptr) {
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	//CREATING A RENDERER
+	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == nullptr) {
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	//LOADING A BITMAP IMAGE
+	std::string imagePath = getResourcePath("Lesson1") + "hello.bmp";
+	SDL_Surface* bmp = SDL_LoadBMP(imagePath.c_str());
+	if (bmp == nullptr) {
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
+	SDL_FreeSurface(bmp);
+	if (tex == nullptr) {
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+
+	return 0;
+}
+
+//https://www.willusher.io/sdl2%20tutorials/2013/08/17/lesson-1-hello-world
